3e9cfb4afaa88ec5596bf051c688929c4e5f5be5
jason <yihanwang.wang@mail.utoronto.ca>
2024-09-12 19:02:35 -0400

    New Multiples file

---
+++ src/Multiples.java
@@ -0,0 +1,22 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 1000){
+
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 && divisibleBy5) {
+                System.out.println(i);
+
+            } else if (divisibleBy3) {
+                System.out.println(i);
+
+            }else if (divisibleBy5) {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
